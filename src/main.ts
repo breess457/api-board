19d06d2c65ce2a5339340872901a58c5d3b04dc8
sobree mama <sobreemama@MacBook-Air-khxng-sobree.local>
2025-01-28 04:21:37 +0700

    add cors OPTIONS

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin:process.env.URL_FRONTEND,
-    methods:'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods:'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders:'Content-Type, Authorization',
     credentials: true
   })
